7c1ccae62d8ab552a80072eae2b1d95e5981e34b
Lucas01PL <lucas01_ce@hotmail.com>
2015-11-07 17:55:17 -0200

    Primeiro Commit
    Alterações após realização da segunda aula.

---
+++ src/br/com/caelum/financas/teste/TesteJPA.java
@@ -0,0 +1,29 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteJPA {
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		conta.setTitular("Lucas Pereira de Lima");
+		conta.setBanco("HSDBC");
+		conta.setNumero("123345");
+		conta.setAgencia("321");
+		EntityManager manager = new JPAUtil().getEntityManager();
+		
+		double inicio = System.currentTimeMillis();
+		manager.getTransaction().begin();
+		manager.persist(conta);
+		manager.getTransaction().commit();
+		manager.close();
+		double fim = System.currentTimeMillis();
+	    System.out.println("Executado em: " + (fim - inicio)/1000 + "s");
+	}
+
+}
